a4236f1cb42cf5ce02982b57989511532f18d3ce
Robert Edwardes <rdedwardes@gmail.com>
2019-01-05 04:30:22 -0500

    remove quotes around env.MONGODB

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const MongoStore = require('connect-mongo')(session);
 const app = express();
 const port = process.env.PORT || 8080;
 
-mongoose.connect("process.env.MONGODB_URI", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection errors'))
